ce3133bf723ca07090ec1eb599a72e3dce34ef06
Tsunekawa <tsunekaw@slis.tsukuba.ac.jp>
2013-03-20 22:20:59 +0900

    generate book controller

---
+++ app/controllers/book.rb
@@ -0,0 +1,25 @@
+MyBookList.controllers :book do
+  # get :index, :map => "/foo/bar" do
+  #   session[:foo] = "bar"
+  #   render 'index'
+  # end
+
+  # get :sample, :map => "/sample/url", :provides => [:any, :js] do
+  #   case content_type
+  #     when :js then ...
+  #     else ...
+  # end
+
+  # get :foo, :with => :id do
+  #   "Maps to url '/foo/#{params[:id]}'"
+  # end
+
+  # get "/example" do
+  #   "Hello world!"
+  # end
+
+  get :search, :map => "/book/search" do
+    @title = "本の検索"
+    render 'book/search'
+  end  
+end
