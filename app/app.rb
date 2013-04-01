812a9930c6799e5c867476f2ed297334a1578df5
Tsunekawa <tsunekaw@slis.tsukuba.ac.jp>
2013-04-01 15:19:06 +0900

    Assets Pipeline の導入

--- app/app.rb
+++ app/app.rb
@@ -3,6 +3,8 @@ class MyBookList < Padrino::Application
   register Padrino::Rendering
   register Padrino::Mailer
   register Padrino::Helpers
+  register Padrino::Sprockets
+  sprockets # :url => 'assets', :root => app.root
 
   enable :sessions
 
