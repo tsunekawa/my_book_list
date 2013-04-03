cb7ebb126cebee4f7f9a355e5528b9a517e504f6
Tsunekawa <tsunekaw@slis.tsukuba.ac.jp>
2013-04-03 16:32:32 +0900

    Account と Book (AmazonItem) との間に多対多の関係を張った

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 2) do
+ActiveRecord::Schema.define(:version => 3) do
 
   create_table "accounts", :force => true do |t|
     t.string   "name"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(:version => 2) do
     t.string   "asin",       :null => false
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "having_books", :force => true do |t|
+    t.integer  "account_id",     :null => false
+    t.integer  "amazon_item_id", :null => false
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
   end
 
 end
