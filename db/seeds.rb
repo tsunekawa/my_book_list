6a1946bfd2fa1739440ae7919b49cee274e9aaef
Tsunekawa <tsunekaw@slis.tsukuba.ac.jp>
2013-04-08 15:45:41 +0900

    seed登録時に既に登録されているデータは無視するよう改修

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,28 +5,42 @@
 #   name = shell.ask("What's your name?")
 #   shell.say name
 #
-email     = shell.ask "Which email do you want use for logging into admin?"
-password  = shell.ask "Tell me the password to use:"
 
-shell.say ""
+if Account.count<=0 then
+  email     = shell.ask "Which email do you want use for logging into admin?"
+  password  = shell.ask "Tell me the password to use:"
 
-account = Account.create(:email => email, :name => "Foo", :surname => "Bar", :password => password, :password_confirmation => password, :role => "admin")
-
-if account.valid?
-  shell.say "================================================================="
-  shell.say "Account has been successfully created, now you can login with:"
-  shell.say "================================================================="
-  shell.say "   email: #{email}"
-  shell.say "   password: #{password}"
-  shell.say "================================================================="
-else
-  shell.say "Sorry but some thing went wrong!"
   shell.say ""
-  account.errors.full_messages.each { |m| shell.say "   - #{m}" }
+
+  account = Account.create(:email => email, :name => "Foo", :surname => "Bar", :password => password, :password_confirmation => password, :role => "admin")
+
+  if account.valid?
+    shell.say "================================================================="
+    shell.say "Account has been successfully created, now you can login with:"
+    shell.say "================================================================="
+    shell.say "   email: #{email}"
+    shell.say "   password: #{password}"
+    shell.say "================================================================="
+  else
+    shell.say "Sorry but some thing went wrong!"
+    shell.say ""
+    account.errors.full_messages.each { |m| shell.say "   - #{m}" }
+  end
+
+  shell.say ""
+else
+  shell.say "admin account has already been created." 
 end
 
-shell.say ""
+pages_yml = YAML.load_file(File.join(File.dirname(__FILE__),"pages.yml"))
+pages     = pages_yml.inject(Array.new) do |list,data|
+  if Page.where(:slag=>data[:slag]).first.blank? then
+    list << Page.create(data)
+    shell.say("page:#{data[:title]} was created.")
+  else
+    shell.say("page:#{data[:title]} has been already created.")
+  end
+  list
+end
 
-pages_yml = YAML.load_file(File.join(FILE.dirname(__FILE__),"pages.yml"))
-pages     = pages_yml.map{|data| Page.find(:slag=>data[:slag]).present? ? Page.create(data) : nil }.compact
 shell.say "create #{pages.count} pages"
