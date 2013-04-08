d3a7892e7e65e81683f3e9232b324a45b6c600e4
Tsunekawa Mao <tsunekaw@slis.tsukuba.ac.jp>
2013-04-08 18:26:21 +0900

    サブディレクトリ下で運用時のリンクURLが正しくなるように改修

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -7,11 +7,11 @@ MyBookList.controllers :sessions do
   post :create do
     if account = Account.authenticate(params[:email], params[:password])
       set_current_account(account)
-      redirect '/'
+      redirect url_for(:top, :top)
     elsif Padrino.env == :development && params[:bypass]
       account = Account.first
       set_current_account(account)
-      redirect '/'
+      redirect url_for(:top, :top)
     else
       params[:email], params[:password] = h(params[:email]), h(params[:password])
       flash[:warning] = "Login or password wrong."
